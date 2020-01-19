39da1bf84c67e23fde35f734ad2e7639b5692c51
pythonguru101 <pythonguru101@gmail.com>
2020-01-18 22:01:47 -0800

    change admin

--- apps/achievements/admin.py
+++ apps/achievements/admin.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from django.conf import settings
 from .models import *
 
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
-        'slug': ('name',) 
+        'slug': ('name',)
     }
     list_display = (
         'name',
@@ -14,14 +15,16 @@ class CategoryAdmin(admin.ModelAdmin):
         'sort',
         'is_active',
     )
-    
+
     list_editable = (
         'sort', 'is_active',
     )
 
+
 class RecordProofAdminInline(admin.TabularInline):
     model = RecordProof
     extra = 0
+
 
 class RecordAdmin(admin.ModelAdmin):
     inlines = (
@@ -36,11 +39,13 @@ class RecordAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     search_fields = ('user__email', 'user__first_name', 'user__last_name')
     list_per_page = 30
-    
+
     def get_user(self, inst):
         user = inst.user
         return "%s %s [%s]" % (user.first_name, user.last_name, user.email)
+
     get_user.short_description = _(u'пользователь')
-    
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Record, RecordAdmin)
