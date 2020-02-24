4946a7d39b0f0ba37290dd6e6bde4e96b9e33aa8
pythonguru101 <pythonguru101@gmail.com>
2020-02-23 17:46:01 -0800

    fix core model

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import ugettext as _
 
 
 class MetaPage(models.Model):
-    meta_title = models.CharField(_(u'Заголовок'), blank=True, 
+    meta_title = models.CharField(_(u'Заголовок'), blank=True,
                                   max_length=200)
-    meta_keywords = models.CharField(_(u'Ключевые слова'), blank=True, 
+    meta_keywords = models.CharField(_(u'Ключевые слова'), blank=True,
                                      max_length=300)
     meta_description = models.TextField(_(u'Описание'), blank=True)
 
