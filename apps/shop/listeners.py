f415f392a0f64789d8eed1fd5fbd99ad41fbe18c
pythonguru101 <pythonguru101@gmail.com>
2020-03-17 19:13:25 -0700

    fix shop listener

--- apps/shop/listeners.py
+++ apps/shop/listeners.py
@@ -1,12 +1,12 @@
 # encoding: utf-8
 
 def add_item(sender, item, price, quantity, **kwargs):
-    #print 'append', item, price, quantity
+    # print 'append', item, price, quantity
     pass
 
 
 def remove_item(sender, item, quantity, **kwargs):
-    #print 'remove', item, quantity
+    # print 'remove', item, quantity
     pass
 
 
